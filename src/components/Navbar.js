c576ad0d91b165366136b22454a1fbde05dc3a37
Kruttika Abhyankar <109844844+Kruttikaaa@users.noreply.github.com>
2023-10-27 13:10:54 +0530

    css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">
-        <h1>Food App</h1>
+       <h1>Kwick Kravings<img src="/taco.png" alt="Profile" className='small-image'/></h1>
       </div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/cart">Cart</Link> </li>
-        <li><Link to="/user-profile"><img src="profile-icon.png" alt="Profile" /></Link></li>
+        <li><Link to="/user-profile">Profile</Link></li>
         <li><Link to="/about-us">About Us</Link></li>
         {/* Add more navigation links as needed */}
       </ul>
