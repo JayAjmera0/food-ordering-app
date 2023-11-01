c13da79343e3e03ebdf1e079cad134d3aa5ff94b
Kruttika Abhyankar <109844844+Kruttikaaa@users.noreply.github.com>
2023-11-01 18:43:01 +0530

    css

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -56,15 +56,164 @@ const RestaurantMenu = () => {
         { id: 14, name: 'Thali', description: 'Enjoy a delectable Indian Thali with an array of flavors on one platter.', price: 499.99, imageUrl: '/indian.jpg', rating: 4.6 },
         { id: 15, name: 'Gulab-Jamun', description: 'Indulge in the sweetness of Gulab Jamun, a classic Indian dessert.', price: 199.50, imageUrl: '/ind4.jpg', rating: 4.7 },
         { id: 16, name: 'Samosa', description: 'Savor the crispy and flavorful Samosa, a popular Indian snack.', price: 179.00, imageUrl: '/ind5.jpg', rating: 4.5 }
-    
-    
-        // Add more main course items as needed
       ]},
-      // Add more categories as needed
+  
     ]},
-
-  ];
-
+    {
+      id: 4,
+      name: 'Sushi Haven',
+      rating: 4.8,
+      menu: [
+        { category: 'Appetizers', items:[
+          { id: 11, name: 'Nigiri Sushi', description: 'Delight in the simplicity and flavor of Nigiri Sushi.', price: 399.00, imageUrl: '/sushi1.jpg', rating: 4.7 },
+          { id: 12, name: 'Sashimi Platter', description: 'Experience the freshness of Sashimi with a variety of fish.', price: 159.99, imageUrl: '/sushi2.jpg', rating: 4.5 },
+          { id: 13, name: 'Cavier Sushi', description: 'Warm up with comforting cavier Sushi.', price: 179.50, imageUrl: '/sushi3.jpg', rating: 4.6 }
+          // Add more sushi appetizer items as needed
+        ]},
+        { category: 'Main Courses', items:[
+          { id: 14, name: 'Sushi Combo', description: 'Enjoy a delightful Sushi Combo with a variety of rolls and flavors.', price: 499.99, imageUrl: '/sushi.jpg', rating: 4.6 },
+          { id: 15, name: 'Tempura Roll', description: 'Savor the crispy and delicious Tempura Roll with your favorite fillings.', price: 199.50, imageUrl: '/sushi4.jpg', rating: 4.7 },
+          { id: 16, name: 'Dragon Roll', description: 'Indulge in the complex flavors of the Dragon Roll, a sushi masterpiece.', price: 179.00, imageUrl: '/sushi5.jpg', rating: 4.5 }
+          // Add more sushi main course items as needed
+        ]},
+      ],
+    },
+    
+    {
+      id: 5,
+      name: 'Burger Junction',
+      rating: 4.8,
+      menu: [
+        {
+          category: 'Appetizers',
+          items: [
+            {
+              id: 11,
+              name: 'Classic Cheeseburger',
+              description: 'Indulge in the classic flavors of a cheeseburger, a timeless favorite.',
+              price: 399.00,
+              imageUrl: '/b1.jpg',
+              rating: 4.7,
+            },
+            {
+              id: 12,
+              name: 'Spicy Burger',
+              description: 'Experience the perfect blend of spice with our Spicy Burger.',
+              price: 159.99,
+              imageUrl: '/b2.jpg',
+              rating: 4.5,
+            },
+            {
+              id: 13,
+              name: 'Veggie Burger',
+              description: 'Delight in the goodness of a Veggie Burger, a tasty and nutritious choice.',
+              price: 179.50,
+              imageUrl: '/b3.jpg',
+              rating: 4.6,
+            }
+            // Add more burger items as needed
+          ],
+        },
+        {
+          category: 'Main Courses',
+          items: [
+            {
+              id: 14,
+              name: 'Aloo Pattie Burger',
+              description: 'Enjoy the rich and savory flavors of our Aloo Pattie Burger.',
+              price: 499.99,
+              imageUrl: '/b4.jpg',
+              rating: 4.6,
+            },
+            {
+              id: 15,
+              name: 'Mushroom Swiss Burger',
+              description: 'Indulge in the combination of mushrooms with our Mushroom Swiss Burger.',
+              price: 199.50,
+              imageUrl: '/burger.jpg',
+              rating: 4.7,
+            },
+            {
+              id: 16,
+              name: 'BBQ Pulled Burger',
+              description: 'Savor the smoky and tender BBQ Pulled Burger, a mouthwatering choice.',
+              price: 179.00,
+              imageUrl: '/b1.jpg',
+              rating: 4.5,
+            }
+            // Add more burger main course items as needed
+          ],
+        },
+      ],
+    },
+    
+    {
+      id: 6,
+      name: 'Chai Chai Cafe',
+      rating: 4.8,
+      menu: [
+        {
+          category: 'Appetizers',
+          items: [
+            {
+              id: 11,
+              name: 'Ginger Tea',
+              description: 'Indulge in the simplicity and flavor of Ginger Tea.',
+              price: 399.00,
+              imageUrl: '/tea1.jpg',
+              rating: 4.7,
+            },
+            {
+              id: 12,
+              name: 'Chai Latte',
+              description: 'Experience the rich and creamy Chai Latte, a classic tea delight.',
+              price: 159.99,
+              imageUrl: '/tea2.jpg',
+              rating: 4.5,
+            },
+            {
+              id: 13,
+              name: 'Peppermint Tea',
+              description: 'Delight in the refreshing and invigorating Peppermint Tea.',
+              price: 179.50,
+              imageUrl: '/tea3.jpg',
+              rating: 4.6,
+            }
+            // Add more tea appetizer items as needed
+          ],
+        },
+        {
+          category: 'Main Courses',
+          items: [
+            {
+              id: 14,
+              name: 'Tea Sampler',
+              description: 'Enjoy a delightful Tea Sampler with a variety of tea flavors and aromas.',
+              price: 499.99,
+              imageUrl: '/tea4.jpg',
+              rating: 4.6,
+            },
+            {
+              id: 15,
+              name: 'Chamomile Tea',
+              description: 'Savor the soothing and calming Chamomile Tea, a perfect choice for relaxation.',
+              price: 199.50,
+              imageUrl: '/tea5.jpg',
+              rating: 4.7,
+            },
+            {
+              id: 16,
+              name: 'Kulhad Chai',
+              description: 'Indulge in the bold and robust flavors of Kulhad Chai, a classic tea selection.',
+              price: 179.00,
+              imageUrl: '/chai.png',
+              rating: 4.5,
+            }
+            // Add more tea main course items as needed
+          ],
+        },
+      ],
+    }    ]
   useEffect(() => {
     // Find the restaurant by ID from the data
     const selectedRestaurant = restaurantData.find(rest => rest.id === parseInt(id, 10));
